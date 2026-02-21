f7fc40bcd9d5a0dd0d4fe67b905bbd3c1c195f28
SHIVANSHI SINGH <shivanshisingh1008@gmail.com>
2026-02-21 18:37:59 +0530

    SCRIPT FILE LINKING

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("hello world");
+document.write("hello world");
+var num=10
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="riya malik";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+  console.log("true");
+}
+else{
+  console.log("false");
+}
+var a=100; var b=13; var c=10; var linebreak="<br>";
+document.write("a+b+c");
+result=a+b+c;
+document.write(Result);
+document.write(linebreak);
