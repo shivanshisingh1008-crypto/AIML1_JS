a20bed2086b1acf160fadf0fd9ae225f1541576f
SHIVANSHI SINGH <shivanshisingh1008@gmail.com>
2026-02-23 15:42:12 +0530

    script linking

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("hello world");
 document.write("hello world");
+
 var num=10
 console.log(num);
 console.log(typeof(num));
@@ -25,8 +26,64 @@ if(sym1==sym2){
 else{
   console.log("false");
 }
+
 var a=100; var b=13; var c=10; var linebreak="<br>";
 document.write("a+b+c");
-result=a+b+c;
+result= a+b+c;
 document.write(Result);
 document.write(linebreak);
+
+var a= 10;
+var b= 20;
+var linebreak= "<br/>";
+
+document.write("(a==b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a!=b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>=b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<=b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<b)>=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+var count;
+document.write("strating loop" + "<br/>");
+for(count=0; count<10; count++){
+  document.write("current count:"+count);
+  document.write("<br/>")
+
+}
+document.write("loop stopped");
+
+
+
+const person={
+  fname:"AJAY",
+  lname:"SINGH",
+  age:25
+};
+for (let x in person){
+  console.log("personal details:"+ x +":" + person[x])
+}
+
