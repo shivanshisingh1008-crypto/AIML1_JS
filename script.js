7512a70c9567253af661290942317aaa93225b9a
SHIVANSHI SINGH <shivanshisingh1008@gmail.com>
2026-03-11 22:20:00 +0530

    function

--- script.js
+++ script.js
@@ -112,36 +112,7 @@ switch(grade){
 }
 console.log("exiting switch block");
 
-function myFunction(){
-  alert("hello world")
-}
 
-function sayHello(name,age){
-  console.log(name + " is " + age +" year old");
-}*/
-
-
-/*var res=mul(12,30);
-function mul(x,y){
-  return x*y;
-}
-console.log(res);
-
-//normal function
-function add (a,b){
-  console.log(a+b);
-}
-add(10,20);
-
-//arrow function
-var add2=(a,b)=>{
-  console.log(a+b);
-};
-add2(102,20);
-
-//arrow function for single line parameters
-var add3=(a,b)=> console.log(a+b);
-add(30,20);*/
 
 
 var person={
