79e5b0eed16a5c8476a4d0e00e0c1f711d525a22
SHIVANSHI SINGH <shivanshisingh1008@gmail.com>
2026-03-11 22:17:38 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,38 @@
+var a= 10;
+var b= 20;
+var linebreak= "<br/>";
+
+document.write("(a==b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+
+document.write("(a!=b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+
+document.write("(a>=b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+
+document.write("(a<=b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+
+document.write("(a>b)=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
+
+
+document.write("(a<b)>=>");
+result=(a==b);
+document.write(result);
+document.write(linebreak);
--- script.js
+++ script.js
@@ -33,39 +33,7 @@ result= a+b+c;
 document.write(Result);
 document.write(linebreak);
 
-var a= 10;
-var b= 20;
-var linebreak= "<br/>";
 
-document.write("(a==b)=>");
-result=(a==b);
-document.write(result);
-document.write(linebreak);
-
-document.write("(a!=b)=>");
-result=(a==b);
-document.write(result);
-document.write(linebreak);
-
-document.write("(a>=b)=>");
-result=(a==b);
-document.write(result);
-document.write(linebreak);
-
-document.write("(a<=b)=>");
-result=(a==b);
-document.write(result);
-document.write(linebreak);
-
-document.write("(a>b)=>");
-result=(a==b);
-document.write(result);
-document.write(linebreak);
-
-document.write("(a<b)>=>");
-result=(a==b);
-document.write(result);
-document.write(linebreak);
 
 var count;
 document.write("strating loop" + "<br/>");
